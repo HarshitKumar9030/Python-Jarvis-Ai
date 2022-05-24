bc9395d1e5fbf1e230045d0ba060a1d3afac4c18
Harshit Kumar <93194961+HarshitKumar9030@users.noreply.github.com>
2022-05-24 10:38:18 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import pyttsx3  #pip install pyttsx3
+import datetime  #module
+import speech_recognition as sr
+import wikipedia
+import smtplib
+import webbrowser as wb
+import os  #inbuilt
+import pyautogui
+import psutil  #pip install psutil
+import pyjokes  # pip install pyjokes
+import requests, json  #inbuilt
